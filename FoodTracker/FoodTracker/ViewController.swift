264d2dcd547c300ecd9446a8ca9d2411837fbbab
young4chan <young.chan@live.com>
2016-07-10 17:53:23 +0800

    Add files via upload
    First patch of project uploaded.

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by 陈阳 on 10/7/16.
+//  Copyright © 2016 Young's. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
